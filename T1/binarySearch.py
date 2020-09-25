a4dfddb5278f1a995c8e35befd381b85e9dca655
deskobar <david.escobar@ug.uchile.cl>
2020-09-25 16:21:33 -0300

    binarySearch fancy

--- T1/binarySearch.py
+++ T1/binarySearch.py
@@ -3,24 +3,19 @@
 # Requiere que la propiedad sea "monótona": es falsa hasta cierto punto. Después de ese punto y en adelante es siempre verdadera.
 # Busca el primer valor entre l y h que hace a la propiedad verdadera.
 from pathlib import Path
-
+import sys
 def get_len(fn):
     kB = Path(fn).stat().st_size
     return kB//10
 
-path_p = "P.txt"
-path_t = "T.txt"
-
-P = []
-with open(path_p, 'r') as fp:
+def binarySearch(path_p, path_t):
+  P = []
+  with open(path_p, 'r') as fp:
     P = [int(line.strip()) for line in fp]
-fp.close()
-
-ft = open(path_t, 'r')
-
-
-output = open("output.txt", "w+")
-for p in P:
+  fp.close()
+  ft = open(path_t, 'r')
+  output = open("output.txt", "w+")
+  for p in P:
     l = 0
     h = get_len(path_t)
     m = 0
@@ -34,8 +29,14 @@ for p in P:
         l = m + 1;
     ft.seek(10*l)
     current_num = int(ft.read(9))
-    if p == current_num: 
-        text = "".join([str(current_num),"\n"])
+    if p == current_num:
+        num_str = str(current_num) 
+        num_str_zeros = num_str.zfill(9)
+        text = "".join([num_str_zeros,"\n"])
         output.write(text)
-output.close()
-ft.close()
+  output.close()
+  ft.close()
+  return 0
+
+arg = sys.argv
+binarySearch(arg[1], arg[2])
