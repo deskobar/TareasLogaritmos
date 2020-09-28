288638b94cca6a565ae3bd77f1e3a2ec0820c9e8
deskobar <david.escobar@ug.uchile.cl>
2020-09-28 10:34:14 -0300

    new enunciate updated

--- T1/binarySearch.py
+++ T1/binarySearch.py
@@ -7,23 +7,28 @@ import sys
 
 def get_len(fn):
     kB = Path(fn).stat().st_size
-    return kB//10
+    line_size = 10
+    len_file = kB//line_size
+    return len_file
 
 def binarySearch(path_p, path_t):
+  # Files section
   P = []
   with open(path_p, 'r') as fp:
     P = [int(line.strip()) for line in fp]
   fp.close()
   ft = open(path_t, 'r')
-  output = open("output.txt", "w+")
+  output = open("output.txt", "w+") # Could be a huge file, so we don't read nothing
+
+  # We need to iterate over P loaded in memory
   for p in P:
     l = 0
     h = get_len(path_t)
     m = 0
     while (l < h):
       m = l + (h - l)//2
-      ft.seek(10*m)
-      current_num = int(ft.read(9))
+      ft.seek(10*m) # Move the pointer to the line we're interested 
+      current_num = int(ft.read(9)) # And read
       if p <= current_num:
         h = m
       else:
