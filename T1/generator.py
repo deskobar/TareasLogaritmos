5103b4e8118ad5536f8488a75da56816b9de60d9
deskobar <david.escobar@ug.uchile.cl>
2020-09-29 16:22:01 -0300

    refactor to use utils

--- T1/generator.py
+++ T1/generator.py
@@ -7,18 +7,21 @@ P_PATH = "P_test.txt"
 T_PATH = "T_test.txt"
 T_PATH_tmp = ".tmp.txt"
 
+MIN = 1
+MAX = 10**9
+
 def generator(p, t):
     ti = time.time()
     print("[*] GENERATING FILES STARTED")
     P = open(P_PATH, "w+")
     T = open(T_PATH_tmp, "w+")
     for i in range(int(p)):
-        p_random = random.randint(1, 10**7)
+        p_random = random.randint(MIN, MAX)
         p_string = str(p_random).zfill(9) + '\n'
         P.write(p_string)
     P.close()
     for j in range(int(t)):
-        t_random = random.randint(1, 10**7)
+        t_random = random.randint(MIN, MAX)
         t_string = str(t_random).zfill(9) + '\n'
         T.write(t_string)
     T.close()
--- T1/indexed_search.py
+++ T1/indexed_search.py
@@ -1,56 +1,46 @@
 from utils import *
 
-
 BLOCK_SIZE = 200
 
-
-def binary_search_modified(arr, low, high, x): 
-    if high >= low: 
-  
-        mid = (high + low) // 2
-   
-        if arr[mid] == x or high == low: 
-            return mid 
-        elif arr[mid] > x: 
-            return binary_search_modified(arr, low, mid - 1, x) 
-        else: 
-            return binary_search_modified(arr, mid + 1, high, x) 
-    else: 
+def binary_search_modified(arr, x, n):
+    low = 0
+    mid = 0
+    high = n
+    while (low < high):
+        mid = low + (high - low) // 2
+        if x <= arr[mid]:
+            high = mid
+        else:
+            low = mid + 1
+    if x < arr[low]:
+        return low - 1
+    elif x == arr[low]:
+        return low
+    else:
         return -1
 
-
-def indexed_search(path_p, path_t):
+def indexedSearch(path_p, path_t):
     P = get_P(path_p)
     T = get_T(path_t)
+    out = get_output("output.txt")
     S = []
-    out = get_output("indexed_output.txt")
-    
-    len_t = get_len(path_t)
+    len_t = get_length_file(path_t)
     B = BLOCK_SIZE
     n_blocks = len_t // B + 1
-
     for i in range(n_blocks):
-        T.seek(LINE_SIZE * i * B)
-        line_str = T.read(9)
+        line_str = read_a_line_from_file(T, i * B)
         if bool(line_str):
             S.append(int(line_str))
-    
-    for line in P:
-        searched = int(line)
-        s_interval_id = binary_search_modified(S, 0, len(S) - 1, searched)
-
-        if s_interval_id >= 0:
-            if searched == S[s_interval_id]:
-                num = str(searched).zfill(9) + '\n'
-                out.write(num)
-            else:
-                T.seek(LINE_SIZE * s_interval_id * B)
-                block = T.read(LINE_SIZE * B).split(sep='\n')
-                for block_number in block:
-                    if bool(block_number) and searched == int(block_number):
-                        num = str(searched).zfill(9) + '\n'
-                        out.write(num)
+    for searched in P:
+        s_interval_id = binary_search_modified(S, searched, len(S) - 1)
+        if s_interval_id != -1:
+            start_reading = LINE_SIZE * s_interval_id * B
+            for block_number in read_many_lines(start_reading, B, T):
+                if searched == int(block_number):
+                    num = str(searched).zfill(9) + '\n'
+                    out.write(num)
     T.close()
     out.close()
 
-execute_search(indexed_search)
+
+execute_search(indexedSearch)
