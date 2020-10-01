90b5a4ae720e6f3d5bdda47341bd05b287d6601b
deskobar <david.escobar@ug.uchile.cl>
2020-10-01 17:21:13 -0300

    hotfix linear_search_plus_merge

--- T1/linear_search_plus_merge.py
+++ T1/linear_search_plus_merge.py
@@ -2,7 +2,7 @@ from utils import execute_search, get_P, get_T, get_output, get_length_file, BLO
 
 def linear_search_plus_merge(file_path_P, file_path_T):
     P = get_P(file_path_P)
-    P = P.sort()
+    P.sort()
     file_T = get_T(file_path_T)
     length_T = get_length_file(file_path_T)
     output = get_output("output_linear_plus_merge.txt")
@@ -15,16 +15,18 @@ def linear_search_plus_merge(file_path_P, file_path_T):
         lines = read_many_lines(start_reading_from, number_of_lines, file_T)
         index_P = 0
         index_T = 0
-        while index_P < len(P) and index_T < len(lines):
+        next = True
+        while index_P < len(P) and index_T < len(lines) and next:
             element_P = P[index_P]
             element_T = lines[index_T]
-            if element_P == element_T:
-                found_element = element_T.zfill(9) + '\n'
-                output.write(found_element)
-            elif element_P < element_T:
+            if element_P > int(element_T):
+                index_T += 1
+            elif element_P < int(element_T):
                 index_P += 1
             else:
-                index_T += 1
+                found_element = element_T.zfill(9) + '\n'
+                output.write(found_element)
+                next = False
     file_T.close()
     output.close()
     return 0
