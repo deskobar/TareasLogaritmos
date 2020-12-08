6d19effe7d874a5dc25e4cd0600a5f50ae42cd36
pisc1s <rupali.lopez@hotmail.com>
2020-12-07 22:24:05 -0300

    experimentation module -wip

--- T3/src/experimentation_module.py
+++ T3/src/experimentation_module.py
@@ -1,1 +1,20 @@
-def generate_Experiment():
+from file_module import generate_file, search_username_in_file
+
+def generate_experiment(Q):
+    result = {}
+    bloom_filter, L_file = generate_file(m, k, n)
+    for _ in range(0, Q):
+        username_query = generate_random_query()
+        response = 'NOT FOUND'
+        if bloom_filter.check(username_query) == 1:
+            element = search_username_in_file(username_query, L_file)
+            if element != '':
+                response = element
+        result[username_query] = response
+    print(response)
+    return result
+
+def generate_random_query():
+    return 'ugjalpxop'
+
+generate_experiment(1)
